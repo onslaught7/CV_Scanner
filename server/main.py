87c09f8d71402ac65b22f4f72e99d80115b8e8b2
Krutibash Mohapatra <137673375+onslaught7@users.noreply.github.com>
2025-04-17 15:28:15 +0530

    cors middleware setup

--- server/main.py
+++ server/main.py
@@ -2,10 +2,29 @@ from fastapi import FastAPI, HTTPException, Depends
 from sqlalchemy.orm import Session
 from server.app.models.database import SessionLocal
 from server.app.routes import auth_routes, coverletter_route, resume_route
+from fastapi.middleware.cors import CORSMiddleware
+import os
+from dotenv import load_dotenv
+
+
+# Load the eenvironment variables
+load_dotenv()
 
 
 # Creating the FastAPI instance
 app = FastAPI(title="CV Scanner API", version="dev_1.0")
+
+
+# Allow cross-origin resource sharing between client and server
+origins = [os.getenv("ORIGIN")]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["POST", "GET", "PUT"],
+    allow_headers=["*"],
+    max_age=900, # specify the ttl for the browser to use when caching our responses, 600 seconds by default
+)
 
 
 # Dependency to get a db session per request
