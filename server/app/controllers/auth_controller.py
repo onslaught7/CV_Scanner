ec41c886345cecdc67de7f808ae86074681b5ceb
Krutibash Mohapatra <137673375+onslaught7@users.noreply.github.com>
2025-04-18 11:18:51 +0530

    endpoints

--- server/app/controllers/auth_controller.py
+++ server/app/controllers/auth_controller.py
@@ -133,11 +133,20 @@ async def login_for_access_token(form_data: Annotated[OAuth2PasswordRequestForm,
         )
     access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
     access_token = create_access_token(
-        data = {"sub": user.username},
+        data = {"sub": user.username}, # we actually pass in the email to here 
         expires_delta = access_token_expires        
     )
     return Token(access_token=access_token, token_type="bearer")
 
+
+@app.get("/user/me/", response_model=User)
+async def read_users_me(current_user: Annotated[User, Depends(get_current_active_user)]):
+    return current_user
+
+
+@app.get("/users/me/items/")
+async def read_own_items(current_user: Annotated[User, Depends(get_current_active_user)]):
+    return [{"item_id": "Foo", "owner": current_user.username}]
 
     
 
